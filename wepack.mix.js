bfcc4e852a1445cf80741c042c06c18194c6eefe
BigewebSolution <81871957+delyn112@users.noreply.github.com>
2024-08-09 02:50:43 +0800

    Added css compiler by laravel mix

---
+++ wepack.mix.js
@@ -0,0 +1,9 @@
+let mix = require("laravel-mix");
+mix.styles([
+    //
+], 'public/css/home.css').setPublicPath('public');
+
+
+mix.scripts([
+    //
+], 'public/js/home.js').setPublicPath('public');
