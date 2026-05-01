ce4b74885ab916efcdfb24f60eb3038740dc1b10
Limahosting <81871957+delyn112@users.noreply.github.com>
2026-05-01 17:46:32 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ header("Access-Control-Allow-Methods: GET, POST, OPTIONS"); // Allow necessary m
 header("Access-Control-Allow-Headers: Content-Type, Authorization"); // Allow specific headers
 
 require(__DIR__.'/../vendor/autoload.php');
-
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
 //Let the application begins to load
 new Loader()
 
